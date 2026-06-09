7c3888bcf7b3baa0fea1bb9cd0c1e9b3fb98801d
Thomas Johnson <islandcity57@gmail.com>
2026-06-08 23:42:00 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+
+        count_s = {}
+        count_t = {}
+
+        for c in s: 
+            if c in count_s:
+                count_s[c] += 1 
+            else:
+                count_s[c] = 1
+
+        for c in t:
+            if c in count_t:
+                count_t[c] += 1
+            else:
+                count_t[c] = 1
+        
+        return count_s == count_t
